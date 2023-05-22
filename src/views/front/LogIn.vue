0251a02272be654767d0a61c4e5d81d0d198a582
Oscar <oscar50048@gmail.com>
2023-05-22 23:03:28 +0800

    login loadinganimation

--- src/views/front/LogIn.vue
+++ src/views/front/LogIn.vue
@@ -5,33 +5,16 @@
         <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
         <form id="form" class="form-signin">
           <div class="form-floating mb-3">
-            <input
-              type="email"
-              class="form-control"
-              id="username"
-              placeholder="name@example.com"
-              required
-              autofocus
-              v-model="user.username"
-            />
+            <input type="email" class="form-control" id="username" placeholder="name@example.com" required autofocus
+              v-model="user.username" />
             <label for="username">Email address</label>
           </div>
           <div class="form-floating">
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="Password"
-              required
-              v-model="user.password"
-            />
+            <input type="password" class="form-control" id="password" placeholder="Password" required
+              autocomplete="current-password" v-model="user.password" />
             <label for="password">Password</label>
           </div>
-          <button
-            class="btn btn-lg btn-primary w-100 mt-3"
-            type="submit"
-            @click.prevent="login"
-          >
+          <button class="btn btn-lg btn-primary w-100 mt-3" type="submit" @click.prevent="login">
             登入
           </button>
         </form>
@@ -42,6 +25,9 @@
 
 <script>
 const { VITE_URL } = import.meta.env;
+import LoadingStore from "@/stores/LoadingStore.js";
+import { mapActions } from "pinia";
+
 export default {
   data() {
     return {
@@ -62,6 +48,10 @@ export default {
         })
         .catch((err) => this.$swal(err.response.data.message));
     },
+    ...mapActions(LoadingStore, ["hideLoading"]),
+  },
+  mounted() {
+    this.hideLoading();
   },
 };
 </script>
